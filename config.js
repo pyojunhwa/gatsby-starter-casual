3cb540c7110b56996fd677b2879d3599fd3df998
pyojunhwa <nberbig@protonmail.com>
2022-05-05 14:38:15 +0200

    global header

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Casual', // <title>
+  siteTitle: 'Freizeitanlage Kaspelwerder', // <title>
   manifestName: 'Casual',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -8,6 +8,6 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-casual/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Gatsby Starter Casual',
-  subHeading: 'A Free Bootstrap 4 based Gatsby Starter',
+  heading: 'Freizeitanlage Kaspelwerder',
+  subHeading: 'Campingstellplätze, Bootverleih und Partyterrasse ',
 };
