6bd97da66dcbabbc54353a772f4583fd9ed0e1ef
pyojunhwa <nberbig@protonmail.com>
2022-07-25 14:08:49 +0200

    Revert "ul mxauto"
    This reverts commit d7ad5809a48caadb2ddd79c213d431e9b7839ea3.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,11 @@ export default function Footer() {
   return (
     <footer className="footer text-center py-5">
       <div className="container">
-          <ul className="mx-auto">
+          <ul>
               <p className="m-0" color="#ffffff">Freizeitanlage - Kaspelwerder</p>
                   <Link className="nav-link"
                         to="/"
                         color="#ffffff"
-                        style="text-decoration:none"
                   >
                       Startseite
                   </Link>
@@ -18,7 +17,6 @@ export default function Footer() {
                       className="nav-link"
                       to="/about"
                       color="#ffffff"
-                      style="text-decoration:none"
                   >
                       Angebote
                   </Link>
@@ -26,7 +24,6 @@ export default function Footer() {
                       className="nav-link"
                       to="/products"
                       color="#ffffff"
-                      style="text-decoration:none"
                   >
                       Anfahrt und Öffnungszeiten
                   </Link>
@@ -34,7 +31,6 @@ export default function Footer() {
                       className="nav-link"
                       to="/store"
                       color="#ffffff"
-                      style="text-decoration:none"
                   >
                       Impressum & Datenschutz
                   </Link>
