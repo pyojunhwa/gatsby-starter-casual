639ee3ab504378a3bdddc5807453a1ed0fe14c5d
pyojunhwa <nberbig@protonmail.com>
2022-07-25 13:09:04 +0200

    footer again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,53 +6,28 @@ export default function Footer() {
     <footer className="footer text-faded text-center py-5">
       <div className="container">
         <p className="m-0" color="fff">Freizeitanlage - Kaspelwerder</p>
-
-          <ul className="navbar-nav text-center mx-auto">
-              <li
-                  className={`nav-item px-lg-4 ${
-                      activeLink === 'home' ? 'active' : ''
-                  }`}
-              >
+          <ul className="text-center mx-auto">
                   <Link className="nav-link text-uppercase text-expanded" to="/">
                       Startseite
                   </Link>
-              </li>
-              <li
-                  className={`nav-item px-lg-4 ${
-                      activeLink === 'about' ? 'active' : ''
-                  }`}
-              >
                   <Link
                       className="nav-link text-uppercase text-expanded"
                       to="/about"
                   >
                       Angebote
                   </Link>
-              </li>
-              <li
-                  className={`nav-item px-lg-4 ${
-                      activeLink === 'products' ? 'active' : ''
-                  }`}
-              >
                   <Link
                       className="nav-link text-uppercase text-expanded"
                       to="/products"
                   >
                       Anfahrt und Öffnungszeiten
                   </Link>
-              </li>
-              <li
-                  className={`nav-item px-lg-4 ${
-                      activeLink === 'store' ? 'active' : ''
-                  }`}
-              >
                   <Link
                       className="nav-link text-uppercase text-expanded"
                       to="/store"
                   >
                       Impressum & Datenschutz
                   </Link>
-              </li>
           </ul>
       </div>
     </footer>
