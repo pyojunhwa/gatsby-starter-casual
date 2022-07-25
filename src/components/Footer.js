d29b28e12d9830c2630ec40ee75d9b44f018d1ba
pyojunhwa <nberbig@protonmail.com>
2022-07-25 15:25:28 +0200

    fixed typo textDecoration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,30 +5,30 @@ export default function Footer() {
   return (
     <footer className="footer text-center py-5">
       <div className="container">
-          <ul className=" mx-auto">
+          <ul style={{ flexDirection:"row"}} className=" mx-auto">
               <p className="m-0" style={{color:"#ffffff"}}>Freizeitanlage - Kaspelwerder</p>
                   <Link
                       className="nav-link"
                       to="/"
-                      style={{textdecoration:"none",color:"#ffffff"}}
+                      style={{textDecoration:"none",color:"#ffffff"}}
                   >
                       Startseite
                   </Link>
                   <Link
                       className="nav-link"
                       to="/about"
-                      style={{textdecoration:"none",color:"#ffffff"}}
+                      style={{textDecoration:"none",color:"#ffffff"}}
                   >
                       Angebote
                   </Link>
                   <Link
-                      style={{textdecoration:"none",color:"#ffffff"}}
+                      style={{textDecoration:"none",color:"#ffffff"}}
                       className="nav-link"
                       to="/products"
                   >
                       Anfahrt und Öffnungszeiten
                   </Link>
-                  <Link style={{textdecoration:"none",color:"#ffffff"}}
+                  <Link style={{textDecoration:"none",color:"#ffffff"}}
                       className="nav-link"
                       to="/store"
                   >
