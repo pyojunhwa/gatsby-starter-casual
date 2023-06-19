c639ff5def85f19dc6061320c8114113b8d02c3b
pyojunhwa <nberbig@protonmail.com>
2023-06-19 12:53:31 +0200

    style: footer change, picture is link now, text to home removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,13 @@ export default function Footer() {
   return (
     <footer className="footer text-center py-5">
       <div className="container">
+        <Link
+          to="/"
+          style={{textDecoration:"none",color:"#ffffff",padding:"12px"}}
+        >
         <img src={wortmarke} style={{height: 160, paddingBottom: 30}}/>
+        </Link>
           <div className="flex-row mx-auto">
-                  <Link
-                      to="/"
-                      style={{textDecoration:"none",color:"#ffffff",padding:"12px"}}
-                  >
-                      Startseite
-                  </Link>
                   <Link
                       to="/angebote"
                       style={{textDecoration:"none",color:"#ffffff",padding:"12px"}}
